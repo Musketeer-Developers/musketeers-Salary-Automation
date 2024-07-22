8397bb4609252f9f927cc7778adbebb57441688a
Rehan Tariq <rehantariq1858@gmail.com>
2024-07-22 11:14:26 +0500

    updated with actual data objects

--- src/pages/Overview/list.tsx
+++ src/pages/Overview/list.tsx
@@ -10,55 +10,9 @@ import {
 } from "@refinedev/antd";
 import { EyeOutlined, SearchOutlined, BookOutlined } from "@ant-design/icons";
 import { Col, Row, Avatar, Flex, Input, Select, Table, Form, Card, Typography } from "antd";
+import employees from '../../../Employees.json';
 
-// Sample data
-const data = [
-  {
-    id: "1",
-    name: "John Doe",
-    designation: "Software Engineer",
-    email: "john.doe@example.com",
-    status: "Active",
-    hours: 40,
-    income: "5000",
-  },
-  {
-    id: "2",
-    name: "Jane Smith",
-    designation: "Project Manager",
-    email: "jane.smith@example.com",
-    status: "Active",
-    hours: 35,
-    income: "7000",
-  },
-  {
-    id: "3",
-    name: "Emily Johnson",
-    designation: "Data Scientist",
-    email: "emily.johnson@example.com",
-    status: "Inactive",
-    hours: 20,
-    income: "3000",
-  },
-  {
-    id: "4",
-    name: "Michael Brown",
-    designation: "UX Designer",
-    email: "michael.brown@example.com",
-    status: "Active",
-    hours: 40,
-    income: "4500",
-  },
-  {
-    id: "5",
-    name: "Sophia Williams",
-    designation: "HR Specialist",
-    email: "sophia.williams@example.com",
-    status: "Active",
-    hours: 30,
-    income: "4000",
-  },
-];
+const data = employees;
 
 export const OverviewPageList = ({ children }: PropsWithChildren) => {  
   return (
@@ -70,11 +24,11 @@ export const OverviewPageList = ({ children }: PropsWithChildren) => {
         }}
       >
         <Table dataSource={data} rowKey="id">
-          <Table.Column title="ID" dataIndex="id" key="id" width={80} sorter />
-          <Table.Column title="Name" dataIndex="name" key="name" width={80} />
-          <Table.Column title="Designation" dataIndex="designation" key="designation" width={80} />
-          <Table.Column title="Email" dataIndex="email" key="email" width={80} />
-          <Table.Column title="Status" dataIndex="status" key="status" width={80} />
+          <Table.Column title="ID" dataIndex="empNo" key="empNo" width={80} sorter />
+          <Table.Column title="Name" dataIndex="Name" key="Name" width={80} />
+          <Table.Column title="Designation" dataIndex="Designation" key="Designation" width={80} />
+          <Table.Column title="Email" dataIndex="Email" key="Email" width={80} />
+          <Table.Column title="Hubstaff" dataIndex="hubstaffEnabled" key="hubstaffEnabled" width={80} />
           <Table.Column title="Hours" dataIndex="hours" key="hours" width={80} />
           <Table.Column title="Income" dataIndex="income" key="income" width={80} align="center" />
           <Table.Column
