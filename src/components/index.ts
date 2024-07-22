6a717b8452bd660cb2136216d25f77727cbf992f
Rehan Tariq <rehantariq1858@gmail.com>
2024-07-22 16:21:20 +0500

    profile page bank button

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { Header } from "./header";
 export { OverviewPageList } from "./../pages/Overview/list";
-export { EmployeeProfile } from "../pages/Overview/profile"
+export { EmployeeProfile } from "../pages/Overview/profile";
+export { PostShow } from "../pages/Overview/show";
--- src/pages/Overview/show.tsx
+++ src/pages/Overview/show.tsx
@@ -1,9 +1,39 @@
+import { Show, useModalForm } from "@refinedev/antd";
+import { Modal, Button, Typography, Divider } from "antd";
+import { BankOutlined } from "@ant-design/icons";
+type Action = "create" | "edit" | "clone" | "show";
 
+export const PostShow: React.FC = () => {
+  const { modalProps, id, show } = useModalForm<Action>({
+    action: "show",
+  });
 
-export const ShowProduct = () => {
+  return (
+    <div>
+      <Button
+        type="primary"
+        size="large"
+        style={{
+          width: "100%",
+          marginTop: 24,
+        }}
+        icon={<BankOutlined />}
+        onClick={() => show()}
+      >
+        See Bank Details
+      </Button>
 
-  // if (isLoading) {
-  //   return <div>Loading...</div>;
-  // }
-
+      <Modal {...modalProps} title="Bank Details" footer={null} width={600} >
+        <Show title="Employee ID" headerButtons={() => null}>
+          <Typography.Title level={5}>Bank : United Bank Limited</Typography.Title>
+          <Typography.Title level={5}>Account Title: Rehan Tariq</Typography.Title>
+          <Typography.Title level={5}>UBAN: PK13UNIL0109000000000001</Typography.Title>
+          <Typography.Title level={5}>Branch: Gulshan-e-Iqbal</Typography.Title>
+          <Typography.Title level={5}>City: Lahore</Typography.Title>
+          <Typography.Title level={5}>Country: Pakistan</Typography.Title>
+          
+        </Show>
+      </Modal>
+    </div>
+  );
 };
