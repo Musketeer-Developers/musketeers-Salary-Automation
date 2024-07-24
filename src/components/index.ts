4642d6a4ad8d7b0f02ceb2e055d83206da92abee
Hassan Muzaffar <hassanmuzaffar439@gmail.com>
2024-07-24 15:37:33 +0500

    update add employee

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export { EmployeeProfile } from "../pages/Overview/profile";
 export { Dailylog } from "../pages/Overview/daily_log";
 export { Monthlylog } from "../components/monthy_detail/monthly";
 export { PostShow } from "../pages/Overview/show";
+export {AddnewEmployee} from "../components/add_new_employee/add_new_emp";
+export {ShowEmployees} from "../components/Show_Employees/show";
