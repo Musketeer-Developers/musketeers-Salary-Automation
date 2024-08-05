47261b91b61516da61399754741d1ffea16c8359
Rehan Tariq <rehantariq1858@gmail.com>
2024-08-05 12:51:36 +0500

    Header + Salary List + Loading on all pages

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,16 +1,24 @@
 import type { RefineThemedLayoutV2HeaderProps } from "@refinedev/antd";
 import { useGetIdentity } from "@refinedev/core";
-import { Layout as AntdLayout, Space, Typography, Button, Input, Flex, theme } from "antd";
+import {
+  Layout as AntdLayout,
+  Space,
+  Typography,
+  Button,
+  Input,
+  Flex,
+  theme,
+} from "antd";
 import React, { useContext } from "react";
 import { ColorModeContext } from "../../contexts/color-mode";
-import logo from '../../logo.png';
-import './style.css';
-import { IconMoon } from './icons/icon-moon';
-import { IconSun } from './icons/icon-sun';
+import logo from "../../logo.png";
+import "./style.css";
+import { IconMoon } from "./icons/icon-moon";
+import { IconSun } from "./icons/icon-sun";
 import { useLogout } from "@refinedev/core";
 import { useNavigate } from "react-router-dom";
-import { Tabs } from 'antd';
-import type { TabsProps } from 'antd';
+import { Tabs } from "antd";
+import type { TabsProps } from "antd";
 
 const { Text } = Typography;
 const { useToken } = theme;
@@ -29,53 +37,64 @@ export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
   const { mode, setMode } = useContext(ColorModeContext);
   const { mutate, isLoading } = useLogout();
 
-  const toggleMode = () => setMode(mode === 'light' ? 'dark' : 'light');
+  const toggleMode = () => setMode(mode === "light" ? "dark" : "light");
 
   const navigate = useNavigate();
   const goToHomePage = () => {
-    console.log("goToprofile clicked");
-    navigate('/');
+    console.log("go To Overview clicked");
+    navigate("/");
   };
   const goToAllEmployeesPage = () => {
-    console.log("goToallemployees clicked");
-    navigate('/allemployees');
+    console.log("go To Salary List clicked");
+    navigate("/allemployees");
   };
 
   const onChange = (key: string) => {
-    if(key == "1"){
+    if (key == "1") {
       goToHomePage();
-    }else if(key == "2"){
+    } else if (key == "2") {
       goToAllEmployeesPage();
     }
   };
 
-  const items: TabsProps['items'] = [
+  const items: TabsProps["items"] = [
     {
-      key: '1',
-      label: 'Employees',
+      key: "1",
+      label: "Overview",
     },
     {
-      key: '2',
-      label: 'Overview',
-    }
+      key: "2",
+      label: "Salary List",
+    },
   ];
 
   return (
     <AntdLayout.Header className={`header ${mode}`}>
-      <div className="header-content">
+      <div className="header-content justify-between ">
         <Space>
-          <img src={logo} alt="Logo" onClick={goToHomePage} style={{ cursor: 'pointer', height: '40px' }} />
-          <Text strong className="title" onClick={goToHomePage} style={{ cursor: 'pointer' }}>Automator</Text>
-          <Space style={{ marginTop: "20px" ,marginLeft:"20px"}} >
-            <Tabs
-              size="large"
-              defaultActiveKey="1"
-              items={items}
-              onChange={onChange}
-              onTabClick={(key) => onChange(key)} // Handle tab click
-              style={{}}
-            />
-          </Space>
+          <img
+            src={logo}
+            alt="Logo"
+            onClick={goToHomePage}
+            style={{ cursor: "pointer", height: "40px" }}
+          />
+          <Text
+            strong
+            className="title"
+            onClick={goToHomePage}
+            style={{ cursor: "pointer" }}
+          >
+            Automator
+          </Text>
+        </Space>
+        <Space style={{ marginTop: "25px", marginLeft: "200px" }} >
+          <Tabs
+            size="large"
+            defaultActiveKey="1"
+            items={items}
+            onChange={onChange}
+            onTabClick={(key) => onChange(key)} // Handle tab click
+          />
         </Space>
         <Flex gap="50px" justify="flex-end">
           <Input.Search placeholder="Search..." style={{ width: 200 }} />
@@ -83,13 +102,19 @@ export const Header: React.FC<RefineThemedLayoutV2HeaderProps> = ({
             className="mode-toggle-button"
             shape="circle"
             onClick={toggleMode}
-            icon={mode === 'dark' ? <IconSun /> : <IconMoon />}
+            icon={mode === "dark" ? <IconSun /> : <IconMoon />}
           />
-          <Button type="primary" onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
-            event.preventDefault();
-            mutate();
-          }}
-            disabled={isLoading} danger>Logout</Button>
+          <Button
+            type="primary"
+            onClick={(event: React.MouseEvent<HTMLButtonElement>) => {
+              event.preventDefault();
+              mutate();
+            }}
+            disabled={isLoading}
+            danger
+          >
+            Logout
+          </Button>
         </Flex>
       </div>
     </AntdLayout.Header>
